26d945b154ef248f8be4cb964fd4b3b7dc611c28
goynang <sid@latterfamily.com>
2005-10-20 20:06:51 +0000

    Improved initial data look ups to better handle missing data etc.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::Base
 	end
 	
 	def get_organisation
-		@organisation = Organisation.find_by_url_slug(request.subdomains.first)
-		did_we_find? @organisation
+		@organisation = Organisation.find_by_url_slug(request.subdomains.first)	
 		if @organisation then @sport = @organisation.sport end
+		did_we_find? @organisation
 	end
 	
 	def get_team
@@ -30,32 +30,29 @@ class ApplicationController < ActionController::Base
 	
 	def get_competition
 		@competition = @season.find_competition_by_url_slug @params["competition_slug"]
-		did_we_find? @competition
 		@other_competitions = @season.competitions.find :all, :conditions => ["id!=?" ,@competition.id]
+	  did_we_find? @competition
 	end
 	
 	def get_stage
-		get_competition
-		@stage = @competition.find_stage_by_url_slug @params["stage_slug"]
+		if get_competition
+		 @stage = @competition.find_stage_by_url_slug @params["stage_slug"]
+		end
 		did_we_find? @stage
 	end
 	
 	def get_group
-		get_stage
-		@group = @stage.find_group_by_url_slug @params["group_slug"]
+		if get_stage
+		  @group = @stage.find_group_by_url_slug @params["group_slug"]
+		end
 		did_we_find? @group
 	end
 	
 	private
 	
 	def did_we_find?(thing)
-		if thing.nil?
-			redirect_to "/404.html"
-			false
-		else
-			if thing.respond_to? :title then @title += " - #{thing.title}" end 
-			true
-		end
+	  if thing.respond_to? :title then @title += " - #{thing.title}" end 
+		!(thing.nil? && redirect_to("/404.html"))
 	end
 	
 end
