804fdcc7eb431ff96639b584d21c039059114bb6
goynang <sid@latterfamily.com>
2006-07-10 20:55:34 +0000

    Sorted out default test files for models based on views. No fixtures for these as derived from other models.

--- test/unit/team_game_day_test.rb
+++ test/unit/team_game_day_test.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class TeamGameDayTest < Test::Unit::TestCase
-  fixtures :team_game_days
 
   # Replace this with your real tests.
   def test_truth
