c2ac5d56406dd6d39d5b5d17acfd681c30f221d9
goynang <sid@latterfamily.com>
2006-11-17 19:07:35 +0000

    Removed code that was duplicated in environment with a plugin.

--- config/custom_environment.rb
+++ config/custom_environment.rb
@@ -6,12 +6,3 @@ require_dependency 'jcode'
 require_dependency 'transforms'
 
 ExceptionNotifier.exception_recipients = %w(stephen@latterfamily.com)
-
-# Hack to avoid form helpers adding a DIV around fields with errors.
-# We don't want them damn it!
-module ActionView
-  class Base
-    @@field_error_proc = Proc.new{ |html_tag, instance| "<span class=\"problem\">#{html_tag}</span>" }
-    cattr_accessor :field_error_proc
-  end
-end
