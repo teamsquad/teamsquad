0bc11c1c3582f1f3fbf47c857c6d3f75221303b4
goynang <sid@latterfamily.com>
2006-11-18 00:53:36 +0000

    Addition of acts_as_sluggable has meant some old string transformations not required. Tidied up some libs as a result and moved some code into helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,22 @@
 module ApplicationHelper
 
   def safely_display(source)
-		result = source
-		simple_format(auto_link(CGI.escapeHTML(result))) 
-	end  
+    result = source
+    simple_format(auto_link(CGI.escapeHTML(result))) 
+  end  
+  
+  def nofollowify(text)
+    text.gsub(/<\s*a\s*(.+?)>/i, '<a \1 rel="nofollow">')
+  end
+
+  def strip_html(text)
+    attribute_key = /[\w:_-]+/
+    attribute_value = /(?:[A-Za-z0-9]+|(?:'[^']*?'|"[^"]*?"))/
+    attribute = /(?:#{attribute_key}(?:\s*=\s*#{attribute_value})?)/
+    attributes = /(?:#{attribute}(?:\s+#{attribute})*)/
+    tag_key = attribute_key
+    tag = %r{<[!/?\[]?(?:#{tag_key}|--)(?:\s+#{attributes})?\s*(?:[!/?\]]+|--)?>}
+    text.gsub(tag, '').gsub(/\s+/, ' ').strip
+  end
 
 end
--- config/custom_environment.rb
+++ config/custom_environment.rb
@@ -1,8 +1,4 @@
 # UTF-8 suppport...
 $KCODE = 'u'
 require_dependency 'jcode'
-
-# handy extra bits and bobs...
-require_dependency 'transforms'
-
 ExceptionNotifier.exception_recipients = %w(stephen@latterfamily.com)
