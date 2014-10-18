9748664882459eb09db11da2af8012d45e70c01c
Stephen <goynang@users.noreply.github.com>
2014-10-18 15:13:58 +0100

    Raft of UI fixes and enhancements.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -11,20 +11,28 @@ class CalendarController < AbstractAccountController
   end
   
   def year
+    @titles << 'Calendar'
     @titles << params[:year]
+    @dates = Array.new
+    @competition.match_days.each { |d| @dates << Date.new( d.date.year, d.date.month, d.date.day ) }
+    @months = @competition.match_months
+    render :index
   end
   
   def month
     @date = Date.new( params[:year].to_i, params[:month].to_i, 1 )
+    @titles << 'Calendar'
     @titles << params[:year]
     @titles << @date.strftime('%B')
   end
   
   def day
     @date = Date.new( params[:year].to_i, params[:month].to_i, params[:day].to_i )
+    @titles << 'Calendar'
     @titles << params[:year]
     @titles << @date.strftime('%B')
-    @titles << params[:day].to_i
+    @titles << params[:day].to_i.ordinalize
+    @window_title = "Matches on #{@date.strftime("%A #{params[:day].to_i.ordinalize}")}"
   end
 
 end
